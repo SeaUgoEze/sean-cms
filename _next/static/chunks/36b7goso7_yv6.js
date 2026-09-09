9d7a9d855335d62cf753755801a2b77419f008ea
SeaUgoEze <SeaUgoEze@users.noreply.github.com>
2026-09-09 14:25:38 +0000

    deploy: d7d388f1c7668e06a5925dfdf92a8227e1d3fd2b

---
+++ _next/static/chunks/36b7goso7_yv6.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,63877,t=>{t.v(o=>Promise.all(["static/chunks/2ph1kjwsyff--.js"].map(o=>t.l(o))).then(()=>o(82365)))}]);
